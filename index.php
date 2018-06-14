d5f7491eda42495287d8e293947652e99edd234c
gsayer <gsayer@mcra.fr>
2018-06-14 11:17:57 +0200

    Version 1.0

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php include('inc/header.php'); ?>
+
+			<!-- Banner -->
+				<section id="banner">
+					<!-- <h2>Consulat Honoraire de Cote d’Ivoire dans les départements du Calvados, de l’Orne et de la Manche</h2> -->
+					<!-- <h4><img class="" src="images/ci.png" alt=""></h4> -->
+					<p>Consulat Honoraire de Cote d’Ivoire dans les départements du Calvados, de l’Orne et de la Manche</p>
+					<!-- <ul class="actions">
+						<li><a href="#" class="button">Magna Sed Etiam</a></li>
+					</ul> -->
+				</section>
+
+			<!-- Three -->
+				<div class="wrapper style3">
+					<section class="container">
+						<header class="major">
+							<h2>Informations</h2>
+						</header>
+						<div class="row">
+							<div class="4u 12u(narrower)">
+								<section class="special">
+									<a href="visa.php" class="image fit"><img src="images/visa.jpg" alt="" /></a>
+									<h3>Pour un Visa</h3>
+									<p>Informations sur les visas pour les citoyens français désirant visiter la Côte d’Ivoire.</p>
+									<ul class="actions">
+										<li><a href="visa.php" class="button alt">En savoir plus</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="4u 12u(narrower)">
+								<section class="special">
+									<a href="laisser.php" class="image fit"><img src="images/laisser.jpg" alt="" /></a>
+									<h3>Pour un Laisser-Passer</h3>
+									<p>Informations sur les Laisser-Passer pour les citoyens ivoiriens.</p>
+									<ul class="actions">
+										<li><a href="laisser.php" class="button alt">En savoir plus</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="4u 12u(narrower)">
+								<section class="special">
+									<a href="perte.php" class="image fit"><img src="images/lost.jpg" alt="" /></a>
+									<h3>Déclaration de perte</h3>
+									<p>Informations sur les déclarations de perte de documents ivoiriens.</p>
+									<ul class="actions">
+										<li><a href="perte.php" class="button alt">En savoir plus</a></li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+				</div>
+
+				<?php include('inc/footer.php'); ?>
---
+++ persona.php
@@ -0,0 +1,51 @@
+<?php include('inc/header_options.php'); ?>
+<div class="wrapper style1">
+  <section id="main" class="container">
+    <header class="major">
+      <h2>Qui sommes-nous ?</h2>
+      <!-- <p>Informations sur les déclarations de perte de documents ivoiriens.</p> -->
+    </header>
+
+    <section class="box">
+      <h3>Chef de poste : Mr Gilles Sayer, Consul Honoraire</h3>
+      <div class="row">
+        <div class="6u 12u(narrower)">
+          <div class="9u 12u(narrower)"><span class="image fit"><img src="images/gsayer.png" alt="" /></span></div>
+        </div>
+
+        <div class="6u 12u(narrower)">
+          <header>
+            <h4>Visa à destination de la Côte d’Ivoire</h4>
+            <p>La délivrance de visa se fait uniquement sur rendez-vous en appelant le consulat par téléphone les jours ouvres entre 15h00 et 17h00.</p>
+          </header>
+
+          <h4>Contact</h4>
+          <ul class="labeled-icons">
+            <li>
+              <h3 class="icon fa-map-marker"><span class="label">Adresse</span></h3>
+              Haras de la Huderie, 1839 route de Deauville <br>
+              14950 Glanville <br>
+              France
+            </li>
+            <li>
+              <h3 class="icon fa-phone"><span class="label">Téléphone</span></h3>
+              02 52 68 26 53
+            </li>
+            <li>
+              <h3 class="icon fa-fax"><span class="label">Fax</span></h3>
+              02 22 44 92 60
+            </li>
+            <li>
+              <h3 class="icon fa-envelope"><span class="label">Email</span></h3>
+              <a href="#">consulatcotedivoirecaen@huderie.com</a>
+            </li>
+          </ul>
+
+        </div>
+  </div>
+</section>
+
+  </section>
+    </div>
+
+<?php include('inc/footer.php'); ?>
